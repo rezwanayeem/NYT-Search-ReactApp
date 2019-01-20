66e8a683d15d812bab978f4a63b98b1fe037a638
unknown <rezwanarubaiya@gamil.com>
2019-01-20 12:29:25 -0600

    update

---
+++ client/build/precache-manifest.8f346425abe78b00f796e93c8d1c7fb8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cba395f77a841611dc27",
+    "url": "/static/js/main.cba395f7.chunk.js"
+  },
+  {
+    "revision": "a0fca8f832ae12a80e06",
+    "url": "/static/js/1.a0fca8f8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c3e8230f80b417fd111c6c519777dab4",
+    "url": "/index.html"
+  }
+];
