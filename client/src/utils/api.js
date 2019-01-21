2738caaccc55830a7da841dcfbba5493d8a8472d
unknown <rezwanarubaiya@gamil.com>
2019-01-21 11:17:14 -0600

    update

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,17 +1,17 @@
-import axios from "axios";
-const url = "https://www.googleapis.com/books/v1/volumes?q=";
+// import axios from "axios";
+// const url = "https://www.googleapis.com/books/v1/volumes?q=";
 
-export default {
-  searchBooks: function(query) {
-    return axios.get(url + query);
-  },
-  getBooks: function() {
-    return axios("/api/books");
-  },
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  },
-  deleteBook: function(id) {
-    return axios.delete("api/books/" + id)
-  }
-};
+// export default {
+//   searchBooks: function(query) {
+//     return axios.get(url + query);
+//   },
+//   getBooks: function() {
+//     return axios("/api/books");
+//   },
+//   saveBook: function(bookData) {
+//     return axios.post("/api/books", bookData);
+//   },
+//   deleteBook: function(id) {
+//     return axios.delete("api/books/" + id)
+//   }
+// };
