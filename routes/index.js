1166b4233fcf1a77448d16dd800866270cee4005
unknown <rezwanarubaiya@gamil.com>
2019-01-21 11:10:14 -0600

    update

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api");
 
+// Router for API Routes
 router.use("/api", apiRoutes);
 
+// Sends user to the React Application, only if there are no other routes left.
+router.use(function(req, res) {
+    res.sendFile(path.join(_dirname, "../client/build/index.html"));
+});
 module.exports = router;
